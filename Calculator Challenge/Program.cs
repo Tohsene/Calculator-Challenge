7980193e2f0a82e950222c89ef51f96d98370398
Oluwatosin Peters <Oluwatosin.peters@wemabank.com>
2024-09-13 11:15:41 +0100

    Step 1: Add support for a maximum of 2 numbers using a comma delimiter

--- Calculator Challenge/Program.cs
+++ Calculator Challenge/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello World");
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+var calculator = new StringCalculator();
+
+while (true)
+{
+    Console.WriteLine("Enter numbers to add (or type 'exit' to quit):");
+    string input = Console.ReadLine();
+
+    if (input?.ToLower() == "exit")
+        break;
+
+    try
+    {
+        int result = calculator.Add(input);
+        Console.WriteLine($"Result: {result}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers)) return 0;
+
+        var splitNumbers = numbers.Split(new[] { ',', '\n' }, StringSplitOptions.None);
+        if (splitNumbers.Length > 2)
+            throw new ArgumentException("More than two numbers provided");
+
+        return splitNumbers.Sum(n => int.TryParse(n, out int result) ? result : 0);
+    }
+}
+
+
